5f8533d94b2d1c1a5addda5709345a33489f4021
ut0s <ut0s.dev@gmail.com>
2019-04-29 08:36:03 +0900

    update: AC
    solve TLE,RE problem.
    accumulating gcd result makes code faster.

--- ABC/ABC125/c.cpp
+++ ABC/ABC125/c.cpp
@@ -1,10 +1,11 @@
 /**
-  @date Time-stamp: <2019-04-28 00:30:16 tagashira>
+  @date Time-stamp: <2019-04-29 08:35:45 tagashira>
   @file c.cpp
   @brief https://atcoder.jp/contests/abc125/tasks/abc125_c
 **/
 
 #include <algorithm>
+#include <chrono>
 #include <cmath>
 #include <iomanip>
 #include <iostream>
@@ -35,7 +36,7 @@ int main(int argc, char* argv[]) {
   cin >> N;
   vector<uint> A(0, N);
   uint tmpA;
-  uint i, j;
+  uint i;
 
   for (i = 0; i < N; i++) {
     cin >> tmpA;
@@ -45,31 +46,37 @@ int main(int argc, char* argv[]) {
   // gcd test
   // cout << gcd(1071, 1029) << "\n"; //21
   // mluti gcd test
+  // chrono::system_clock::time_point start, end;
   // cout << multi_gcd({4, 6, 8}) << "\n"; //2
+  // start = chrono::system_clock::now();
+  // for (i = 0; i < 100; i++) {
+  //   multi_gcd({4, 6, 8});
+  // }
+  // end         = chrono::system_clock::now();
+  // double time = static_cast<double>(chrono::duration_cast<chrono::microseconds>(end - start).count() / 1000.0);
+  // printf("gcd time %lf[ms]\n", time / 100);
 
-  uint tmp_gcd  = 0;
-  uint omit_idx = 0;
-  if (N == 2) {
-    cout << *max_element(A.begin(), A.end()) << "\n";
-  } else {
-    for (j = 0; j < N; j++) {
-      vector<uint> omitA;
-
-      for (i = 0; i < N; i++) {
-        if (i != omit_idx) {
-          omitA.push_back(A[i]);
-        }
-      }
-
-      tmp_gcd = max({tmp_gcd, multi_gcd(omitA)});
-
-      // cout << "omit_idx:" << omit_idx << "\n";
-      // cout << "tmp_gcd:" << tmp_gcd << "\n";
-
-      omit_idx++;
-    }
-    cout << tmp_gcd << "\n";
+  // 累積 GCD (左と右両方)
+  vector<uint> left(N + 1, 0), right(N + 1, 0);
+  for (i = 0; i < N; ++i) {
+    left[i + 1] = gcd(A[i], left[i]);
   }
+  for (i = N - 1; i > 0; --i) { //right は集計の際にi+1から始まるからidxの０は不必要
+    right[i] = gcd(A[i], right[i + 1]);
+  }
+
+  // 集計
+  vector<uint> M;
+  for (i = 0; i < N; ++i) {
+    // 左側
+    uint l = left[i];
+
+    // 右側
+    uint r = right[i + 1];
+
+    M.push_back(gcd(l, r));
+  }
+  cout << *max_element(M.begin(), M.end()) << endl;
 
   return 0;
 }
