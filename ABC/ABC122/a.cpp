dfdf08c2ab787f398374c05a38edc3f51cfc2987
ut0s <ut0s.dev@gmail.com>
2019-05-17 21:49:31 +0900

    add: AC https://atcoder.jp/contests/abc122/submissions/5399832

---
+++ ABC/ABC122/a.cpp
@@ -0,0 +1,32 @@
+/**
+  @date Time-stamp: <2019-05-14 21:41:58 tagashira>
+  @file a.cpp
+  @title A - Double Helix
+  @brief https://atcoder.jp/contests/abc122/tasks/abc122_a
+**/
+
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+  char s;
+  cin >> s;
+
+  if (s == 'A') {
+    cout << 'T' << "\n";
+  } else   if (s == 'T') {
+    cout << 'A' << "\n";
+  } else   if (s == 'C') {
+    cout << 'G' << "\n";
+  } else   if (s == 'G') {
+    cout << 'C' << "\n";
+  }
+
+  return 0;
+}
