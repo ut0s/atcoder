9becf1c7aad243547469d9553abdf725c7ef6ded
ut0s <ut0s.dev@gmail.com>
2019-06-02 20:43:01 +0900

    add: m_solutions2019 submission a,b
    URL: https://atcoder.jp/contests/m-solutions2019
    Rank: 17859th
    Rating: 162

---
+++ ABC/m-solutions2019/a.cpp
@@ -0,0 +1,26 @@
+/**
+  @date Time-stamp: <2019-06-02 20:40:20 tagashira>
+  @file a.cpp
+  @title A - Sum of Interior Angles
+  @brief https://atcoder.jp/contests/m-solutions2019/tasks/m_solutions2019_a
+**/
+
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <sstream> // std::ostringstream
+#include <string>
+#include <tuple>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+  uint N;
+  cin >> N;
+
+  cout << (N - 2) * 180 << endl;
+
+  return 0;
+}
---
+++ ABC/m-solutions2019/b.cpp
@@ -0,0 +1,37 @@
+/**
+  @date Time-stamp: <2019-06-02 20:41:48 tagashira>
+  @file b.cpp
+  @title B - Sumo
+  @brief https://atcoder.jp/contests/m-solutions2019/tasks/m_solutions2019_b
+**/
+
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <sstream> // std::ostringstream
+#include <string>
+#include <tuple>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+  string S;
+  cin >> S;
+
+  int count = 0;
+  for (int i = 0; i < S.length(); i++) {
+    if (S[i] == 'o') count++;
+  }
+
+  if (count >= 8) {
+    cout << "YES" << endl;
+  } else if (15 - S.length() >= 8 - count) {
+    cout << "YES" << endl;
+  } else {
+    cout << "NO" << endl;
+  }
+
+  return 0;
+}
