50ef6187ccc33201f92ae1622a0e9fb4a0437e6a
ut0s <ut0s.dev@gmail.com>
2019-06-18 04:01:15 +0900

    update: AC https://atcoder.jp/contests/abc129/submissions/6004003

--- ABC/ABC129/c.cpp
+++ ABC/ABC129/c.cpp
@@ -1,19 +1,10 @@
 /**
-  @date Time-stamp: <2019-06-09 22:41:03 tagashira>
   @file c.cpp
   @title C - Typical Stairs
   @brief https://atcoder.jp/contests/abc129/tasks/abc129_c
 **/
 
-#include <algorithm>
-#include <bitset>
-#include <cmath>
-#include <iomanip>
-#include <iostream>
-#include <sstream> // std::ostringstream
-#include <string>
-#include <vector>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 template <class T>
@@ -24,70 +15,32 @@ void show(vector<T>& v) {
   cout << "\n";
 }
 
-long long facctorial(int k) {
-  long long sum = 1;
-  for (int i = 1; i <= k; ++i) {
-    sum *= i;
-  }
-  return sum;
-}
-
-long long calc_path(int sep) {
-  int i;
-  long long tmpS = 0;
-  if (sep % 2 == 0) {
-    for (i = 0; i <= sep / 2; i++) {
-      int two = i;
-      int one = sep - 2 * i;
-      tmpS    += facctorial(one + two) / (facctorial(one) * facctorial(two));
-    }
-  } else {
-    for (i = 0; i <= (sep - 1 )/ 2; i++) {
-      int one = sep - 2 * i;
-      int two = i;
-      // cout << "Here" << endl;
-      // cout << "1:" << one << endl;
-      // cout << "2:" << two << endl;
-      tmpS += facctorial(one + two) / (facctorial(one) * facctorial(two));
-    }
-  }
-
-  return tmpS;
-}
-
-int main(int argc, char* argv[]) {
-  uint N, M;
+int main() {
+  int N, M;
   cin >> N >> M;
 
-  vector<uint> a(M, 0);
-  for (uint i = 0; i < M; i++) {
-    cin >> a[i];
-  }
-  // show(a);
+  // long long mod = 1000000007;
+  const long long mod = 1e9 + 7;
 
-  for (uint i = 1; i < M; i++) {
-    if (a[i] - a[i - 1] == 1) {
-      cout << "0" << endl;
-      exit(0);
-    }
+  vector<bool> hole(N + 1, false);
+  int a;
+  for (int i = 0; i < M; i++) {
+    cin >> a;
+    hole[a] = true;
   }
+  // show(hole);
 
-  vector<int> separate(M + 1, 0);
-  separate[M] = N - *(a.end() - 1) - 1;
-  for (uint i = 0; i < M; i++) {
-    if (i == 0) {
-      separate[i] = a[i] - 0 - 1;
+  vector<long long> dp(N + 2, 0);
+  dp[N] = 1;
+  for (int i = N - 1; i >= 0; i--) {
+    if (hole[i]) {
+      dp[i] = 0;
     } else {
-      separate[i] = a[i] - a[i - 1] - 2;
+      dp[i] = (dp[i + 1] + dp[i + 2]) % mod;
     }
   }
-  // show(separate);
 
-  long long sum = 0;
-  for (uint i = 0; i < M + 1; i++) {
-    sum += calc_path(separate[i]);
-  }
-  cout << sum%1000000007 << endl;
+  cout << dp[0] << endl;
 
   return 0;
 }
