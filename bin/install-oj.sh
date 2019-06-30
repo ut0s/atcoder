6b8fa70f5329b14c9158acb3a229e1cb2cab346c
ut0s <ut0s.dev@gmail.com>
2019-06-30 12:57:32 +0900

    chore: ignore .problem.json / make symbolic link act / change Makefile dont remove test case

--- bin/install-oj.sh
+++ bin/install-oj.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# @date Time-stamp: <2019-06-02 20:52:41 tagashira>
+# @date Time-stamp: <2019-06-30 12:48:07 tagashira>
 # @file install-oj.sh
 # @brief
 
@@ -8,8 +8,10 @@ DOTFILES="$HOME/dotfiles"
 if [ $(which porg) ]; then
     echo 'porg command found'
     porg -l -p online-judge-tools "pip3 install -U online-judge-tools --user"
+    ln -s $(dirname $0)/act.sh $HOME/bin/act
 else
     echo 'porg command not found ...'
     eval ${DOTFILES}/etc/init/00-install-porg.sh &&\
       porg -l -p online-judge-tools "pip3 install -U online-judge-tools --user"
+    ln -s $(dirname $0)/act.sh $HOME/bin/act
 fi
