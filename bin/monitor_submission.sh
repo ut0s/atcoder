9168ae98a22efb5a311bb2f32eebf33420d75780
ut0s <ut0s.dev@gmail.com>
2019-07-20 23:10:25 +0900

    fix: fix syntax error and modify notify message

--- bin/monitor_submission.sh
+++ bin/monitor_submission.sh
@@ -25,30 +25,49 @@ init(){
 get_status(){
   local url=$1
   local status=$(curl -s $url | grep "judge-status" |cut -d "\"" -f 7 | cut -d "<" -f1 | cut -d ">" -f2)
-
   echo $status
 }
 
-monitor_submission(){
+get_contest_title(){
+  local url=$1
+  local contest_title=$(curl -s $url | grep "contest-title" |cut -d ">" -f 3 | cut -d "<" -f1)
+  echo $contest_title
+}
+
+
+get_problem_name(){
+  local url=$1
+  local problem_name=$(curl -s $url | grep -A2 "<th>Task" |cut -d ">" -f3 | cut -d "<" -f1)
+  echo $problem_name
+}
+
+
+notify_submission(){
   local url=$1
   local status=$(get_status $url)
-  ${NOTIFIY_COMMAND} ${NOTIFIY_COMMAND_OPT} $status $url > /dev/null
+  local contest_title=$(get_contest_title $url)
+  local problem_name=$(get_problem_name $url)
+
+  local message="$contest_title | $problem_name"
+  ${NOTIFIY_COMMAND} ${NOTIFIY_COMMAND_OPT} $status "$message" > /dev/null
   echo $status
 }
 
 main(){
   init
   while true; do
-    res=$(monitor_submission $@)
-    if [ $res = "AC" -o $res = "RE" -o $res = "TLE" -o $res = "WA" ]
+    res=$(get_status $@)
+    if [ $res = "AC" ] || [ $res = "RE" ] || [ $res = "TLE" ] || [ $res = "WA" ]
     then
+      notify_submission $@
       exit 0
     else
       sleep $LAP"s"
-      $count=$(($count + 1))
+      count=$(( $count + 1 ))
       if [ $count -eq 18 ]
       then
         echo "one and half minutes elapsed"
+        exit 1
       fi
 
     fi
@@ -56,3 +75,8 @@ main(){
 }
 
 main $@
+
+# test
+# get_status $@
+# get_contest_title $@
+# get_problem_name $@
