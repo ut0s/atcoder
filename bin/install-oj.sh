3849c982e5fdfb0762e62a4d92571cd50870721b
ut0s <ut0s.dev@gmail.com>
2019-07-27 18:05:06 +0900

    update: install dl_sys command

--- bin/install-oj.sh
+++ bin/install-oj.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# @date Time-stamp: <2019-06-30 12:48:07 tagashira>
+# @date Time-stamp: <2019-07-27 18:04:33 tagashira>
 # @file install-oj.sh
 # @brief
 
@@ -8,10 +8,14 @@ DOTFILES="$HOME/dotfiles"
 if [ $(which porg) ]; then
     echo 'porg command found'
     porg -l -p online-judge-tools "pip3 install -U online-judge-tools --user"
-    ln -s $(dirname $0)/act.sh $HOME/bin/act
+    echo ""
+    ln -sf $(cd $(dirname $0) && pwd)/act.sh $HOME/bin/act && which act
+    ln -sf $(cd $(dirname $0) && pwd)/dl_system_testcase.sh $HOME/bin/dl_sys && which dl_sys
 else
     echo 'porg command not found ...'
     eval ${DOTFILES}/etc/init/00-install-porg.sh &&\
       porg -l -p online-judge-tools "pip3 install -U online-judge-tools --user"
-    ln -s $(dirname $0)/act.sh $HOME/bin/act
+    echo ""
+    ln -sf $(cd $(dirname $0) && pwd)/act.sh $HOME/bin/act && which act
+    ln -sf $(cd $(dirname $0) && pwd)/dl_system_testcase.sh $HOME/bin/dl_sys && which dl_sys
 fi
