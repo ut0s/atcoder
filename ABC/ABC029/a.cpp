86966d9f2eff06c47dc9e725cc9807dded56edbe
ut0s <ut0s.dev@gmail.com>
2019-08-09 15:44:25 +0900

    add: ABC029 A - 複数形 | (AC) https://atcoder.jp/contests/abc029/submissions/6781832

---
+++ ABC/ABC029/a.cpp
@@ -0,0 +1,20 @@
+/**
+  @file a.cpp
+  @title A - 複数形
+  @url https://atcoder.jp/contests/abc029/tasks/abc029_a
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long LL;
+#define ALL(obj) (obj).begin(), (obj).end()
+#define REP(i, N) for (int i = 0; i < (N); ++i)
+
+int main() {
+  string W;
+  cin >> W;
+
+  cout << W + "s" << endl;
+  return 0;
+}
