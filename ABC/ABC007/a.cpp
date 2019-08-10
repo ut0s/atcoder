a61d6b60068a152ee4d3672c66c4dcd7c690ed18
ut0s <ut0s.dev@gmail.com>
2019-08-10 09:12:51 +0900

    add: ABC007 A - 植木算 | (AC) https://atcoder.jp/contests/abc007/submissions/6791254 add: ABC007 B - 辞書式順序 | (AC) https://atcoder.jp/contests/abc007/submissions/6791277

---
+++ ABC/ABC007/a.cpp
@@ -0,0 +1,22 @@
+/**
+  @file a.cpp
+  @title A - 植木算
+  @url https://atcoder.jp/contests/abc007/tasks/abc007_1
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long LL;
+#define ALL(obj) (obj).begin(), (obj).end()
+#define REP(i, N) for (int i = 0; i < (N); ++i)
+
+int main() {
+  int i, j, k, l;
+
+  int N;
+  cin >> N;
+
+  cout << N - 1 << endl;
+  return 0;
+}
---
+++ ABC/ABC007/b.cpp
@@ -0,0 +1,25 @@
+/**
+  @file b.cpp
+  @title B - 辞書式順序
+  @url https://atcoder.jp/contests/abc007/tasks/abc007_2
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long LL;
+#define ALL(obj) (obj).begin(), (obj).end()
+#define REP(i, N) for (int i = 0; i < (N); ++i)
+
+int main() {
+  string A;
+  cin >> A;
+
+  if (A == "a") {
+    cout << "-1" << endl;
+  } else {
+    cout << "a" << endl;
+  }
+
+  return 0;
+}
