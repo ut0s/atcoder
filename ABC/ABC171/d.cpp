bfe6a3b84aa0aaaf65b9991f304786339f2f2ce1
ut0s <ut0s.dev@gmail.com>
2022-04-05 22:14:27 +0900

    add: ABC171 D - Replacing | https://atcoder.jp/contests/abc171/submissions/30741001

---
+++ ABC/ABC171/d.cpp
@@ -0,0 +1,86 @@
+/**
+  @file d.cpp
+  @title D - Replacing
+  @url https://atcoder.jp/contests/abc171/tasks/abc171_d
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+#define ALL(obj) (obj).begin(), (obj).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+#define SZ(x) ((int)(x).size())
+
+#define REPI(i, a, b) for (LL i = LL(a); i < LL(b); ++i)
+#define REP(i, N) for (LL i = LL(0); i < LL(N); ++i)
+#define REPS(i, N) for (int i = 1; i <= (int)(N); i++)
+#define RREP(i, N) for (int i = ((int)(N)-1); i >= 0; i--)
+#define RREPS(i, N) for (int i = ((int)(N)); i > 0; i--)
+
+#define chmax(ret, x) ret = max(ret, x)
+#define chmin(ret, x) ret = min(ret, x)
+
+#define FILL(x, e) memset(x, e, sizeof(x))
+#define ZEROS(x) fill(x, 0)
+
+#define SORT(x) sort(ALL(x))
+#define RSORT(x) sort(RALL(x))
+
+#define UNIQ(x) x.erase(unique(ALL(x)), x.end());
+
+#define BIT(n) (1LL << (n))
+
+#define OUT(x) cout << (x) << "\n"
+#define DEBUG(x) cerr << #x << " : " << x << "\n"
+#define DEBUG2(x, y) cerr << "(" << #x << ", " << #y << ") = (" << x << ", " << y << ")\n";
+
+#define PB push_back
+#define EB emplace_back
+#define MP make_pair
+#define MT make_tuple
+#define FI first
+#define SE second
+
+const int MOD = 1e9 + 7;
+
+void iostream_init() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  cout.setf(ios::fixed);
+  cout.precision(12);
+  // cout.fill('0');
+  // cout.width(3);
+}
+
+int main() {
+  ::iostream_init();
+
+  int N;
+  cin >> N;
+
+  LL sum = 0;
+  vector<int> A(N, 0);
+  map<int, int> count;
+  REP(i, N) {
+    cin >> A[i];
+    count[A[i]]++;
+    sum += A[i];
+  }
+  // cout << "sum: " << sum << endl; // debug
+
+  int Q;
+  cin >> Q;
+
+  int tmpB, tmpC;
+  REP(i, Q) {
+    cin >> tmpB >> tmpC;
+    sum += count[tmpB] * (tmpC - tmpB);
+    cout << sum << endl;
+    count[tmpC] += count[tmpB];
+    count[tmpB] = 0;
+  }
+
+  return 0;
+}
