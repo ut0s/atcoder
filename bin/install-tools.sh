430c08f96136cc603a82619e061a14cc9512d3ec
ut0s <ut0s.dev@gmail.com>
2022-04-11 19:03:20 +0900

    feat: install online-judge-api-client

--- bin/install-tools.sh
+++ bin/install-tools.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# @date Time-stamp: <2019-07-27 18:04:33 tagashira>
 # @file install-oj.sh
 # @brief
 
@@ -8,6 +7,7 @@ DOTFILES="$HOME/dotfiles"
 if [ $(which porg) ]; then
     echo 'porg command found'
     porg -l -p online-judge-tools "pip3 install -U online-judge-tools --user"
+    porg -l -p online-judge-api "pip3 install -U online-judge-api-client"
     echo ""
     ln -sf $(cd $(dirname $0) && pwd)/act.sh $HOME/bin/act && which act
     ln -sf $(cd $(dirname $0) && pwd)/dl_system_testcase.sh $HOME/bin/dl_sys && which dl_sys
@@ -15,7 +15,10 @@ else
     echo 'porg command not found ...'
     eval ${DOTFILES}/etc/init/00-install-porg.sh &&\
       porg -l -p online-judge-tools "pip3 install -U online-judge-tools --user"
+      porg -l -p online-judge-api "pip3 install -U online-judge-api-client"
     echo ""
     ln -sf $(cd $(dirname $0) && pwd)/act.sh $HOME/bin/act && which act
     ln -sf $(cd $(dirname $0) && pwd)/dl_system_testcase.sh $HOME/bin/dl_sys && which dl_sys
 fi
+
+which g++ || ln -fs /opt/homebrew/Cellar/gcc/11*/bin/aarch64-apple-darwin21-g++-11 /usr/local/bin/g++
